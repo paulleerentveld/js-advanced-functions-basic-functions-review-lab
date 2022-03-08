c9c793973fe18f02a839d494947905a4f1722583
paulleerentveld <paul@veldnet.com.au>
2022-03-08 10:41:40 +1000

    Add functions for lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate") {
+   return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flare="*") {
+    return function (x="special") {
+        return `You are ${flare}${x}${flare}!`;
+    }
+
+}
+
+const Calculator = {
+    add:function(a,b) {
+        return a+b;
+    },
+    subtract:function(a,b) {
+        return a-b;
+    },
+    multiply:function(a,b) {
+        return a*b;
+    },
+    divide:function(a,b) {
+        return a/b;
+    }
+}
+
+function actionApplyer(num,array) {
+    for(let i=0;i<array.length;i++) {
+        num=array[i] (num);
+    }
+    return num
+
+}
+
